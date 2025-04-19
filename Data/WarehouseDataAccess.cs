3d4ee60154fe7f2b71357fcaad9e04611caa554e
Xeliozil <lukasz02k@gmail.com>
2025-04-19 18:33:27 +0200

    trying git

---
+++ Data/WarehouseDataAccess.cs
@@ -0,0 +1,72 @@
+﻿using System.Data.SqlClient;
+using System.Collections.Generic;
+using WarehouseManagementUnia.Models;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace WarehouseManagementUnia.Data
+{
+    public class WarehouseDataAccess
+    {
+        private readonly string _connectionString;
+
+        public WarehouseDataAccess()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public List<Product> GetProducts()
+        {
+            var products = new List<Product>();
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("SELECT Id, Name, Quantity, Price FROM Products", connection);
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        products.Add(new Product
+                        {
+                            Id = reader.GetInt32(0),
+                            Name = reader.GetString(1),
+                            Quantity = reader.GetInt32(2),
+                            Price = reader.GetDecimal(3)
+                        });
+                    }
+                }
+            }
+            return products;
+        }
+
+        public void AddProduct(Product product)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand(
+                    "INSERT INTO Products (Name, Quantity, Price) VALUES (@Name, @Quantity, @Price); SELECT SCOPE_IDENTITY();",
+                    connection);
+                command.Parameters.AddWithValue("@Name", product.Name);
+                command.Parameters.AddWithValue("@Quantity", product.Quantity);
+                command.Parameters.AddWithValue("@Price", product.Price);
+                product.Id = Convert.ToInt32(command.ExecuteScalar());
+            }
+        }
+
+        public void DeleteProduct(int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("DELETE FROM Products WHERE Id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", id);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
